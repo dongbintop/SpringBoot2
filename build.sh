09905f2019af482d82ed26f67e83c6fe36dc8382
W23123 <1021720039@qq.com>
2018-10-27 15:58:51 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 mvn clean package -Dmaven.test.skip=true
 
-docker build -t bindong.top/db-test/springboot2:1.0.0 .
-docker push bindong.top/db-test/springboot2:1.0.0
+docker build -t bindong.top/db-test/springboot2:master .
+docker push bindong.top/db-test/springboot2:master
